3c0d6ab20d771a3c0ea22879f65dc93a94cc97fd
Victor Liu <victor.yuchen.liu@gmail.com>
2025-01-28 19:38:13 +0000

    Working alternating PM2.5 and CO2 display.

--- enginair/src/main.cpp
+++ enginair/src/main.cpp
@@ -22,6 +22,7 @@ SensirionI2CScd4x co2Sens;
 // TODO: figure out how to pass in a TwoWire pointer
 void initSEN50();
 void initSCD40();
+void printSensirionError(String message, uint16_t error);
 
 #define PROJECT_NAME "enginAIR"
 enum message_t {
@@ -34,6 +35,8 @@ enum message_t {
 
 bool initDisplay();
 void showMessage(String message, message_t level);
+void showPMValues(float pm1p0, float pm2p5, float pm4p0, float pm10p0);
+void showCO2Values(uint16_t co2, float temp, float humi);
 
 void setup() {
     Wire.setSCL(17);
@@ -56,59 +59,55 @@ void setup() {
     Serial.println("Starting main loop");
 }
 
+int seconds = 0;
 void loop() {
     uint16_t error;
     char errorMessage[256];
-    float pm1p0, pm2p5, pm4p0, pm10p0, temp, nox, voc, humi;
+    float pm1p0, pm2p5, pm4p0, pm10p0, sen_temp, nox, voc, sen_humi;
 
     delay(1000);
+    seconds++;
 
-    error = pmSens.readMeasuredValues(pm1p0, pm2p5, pm4p0, pm10p0, humi, temp, voc, nox);
+    error = pmSens.readMeasuredValues(pm1p0, pm2p5, pm4p0, pm10p0, sen_humi, sen_temp, voc, nox);
     if (error) {
-        Serial.print("Error reading measured values from SEN50: ");
-        errorToString(error, errorMessage, 256);
-        Serial.println(errorMessage);
+        printSensirionError("Error reading measured values from SEN50: ", error);
     } else { 
-        Serial.print("PM1.0: ");
-        Serial.print(pm1p0);
-        Serial.print("\t PM2.5: ");
-        Serial.print(pm2p5);
-        Serial.print("\t PM4.0: ");
-        Serial.print(pm4p0);
-        Serial.print("\t PM10.0: ");
-        Serial.print(pm10p0);
-        Serial.println();
+        // showPMValues(pm1p0, pm2p5, pm4p0, pm10p0);
 
         // SEN50 has no VOC, NOx, humidity or temperature sensor.
     }
 
     // The SCD40 CO2 sensor only produces a new measurement every 5 seconds, 
     // and clears the buffer after reading, so check if data is ready
-    uint16_t co2 = 0;
+    uint16_t co2;
+    float temp, humi;
     bool dataReady = false;
     error = co2Sens.getDataReadyFlag(dataReady);
     if (error) {
-        Serial.print("Couldn't get SCD40 data ready flag: ");
-        errorToString(error, errorMessage, 256);
-        Serial.println(errorMessage);
+        printSensirionError("Couldn't get SCD40 data ready flag: ", error);
         return; // Don't do anything after this if an error occurred at this stage
     }
     
+    // Serial.print("Data ready: ");
+    // Serial.println(dataReady);
     if (dataReady) {
         error = co2Sens.readMeasurement(co2, temp, humi);
         if (error) {
-            Serial.print("Error reading SCD40 measurement: ");
-            errorToString(error, errorMessage, 256);
-            Serial.println(errorMessage);
+            printSensirionError("Error reading measurement from SCD40: ", error);
         } else {
-            Serial.print("CO2: ");
-            Serial.print(co2);
-            Serial.print("\t Temperature: ");
-            Serial.print(temp);
-            Serial.print("\t Humidity: ");
-            Serial.print(humi);
-            Serial.println();
+            // showCO2Values(co2, temp, humi);
         }
+    }
+
+    // Swap between the CO2 and PM values every 5 seconds
+    if (seconds > 5) {
+        showCO2Values(co2, temp, humi);
+    } else {
+        showPMValues(pm1p0, pm2p5, pm4p0, pm10p0);
+    }
+
+    if (seconds >= 10) {
+        seconds = 0;
     }
 }
 
@@ -119,16 +118,12 @@ void initSEN50() {
     char message[256];
     error = pmSens.deviceReset();
     if (error) {
-        Serial.print("Error resetting SEN50: ");
-        errorToString(error, message, 256);
-        Serial.println(message);
+        printSensirionError("Error resetting SEN50: ", error);
     } 
 
     error = pmSens.startMeasurement();
     if (error) {
-        Serial.print("Error starting SEN50 measurement: ");
-        errorToString(error, message, 256);
-        Serial.println(message);
+        printSensirionError("Error starting SEN50 measurement: ", error);
     } else {
         Serial.println("SEN50 measurement started successfully");
     }
@@ -142,21 +137,26 @@ void initSCD40() {
     char message[256];
     error = co2Sens.stopPeriodicMeasurement();
     if (error) {
-        Serial.print("Error stopping SCD40 measurement: ");
-        errorToString(error, message, 256);
-        Serial.println(message);
+        printSensirionError("Error stopping SCD40 measurement: ", error);
     }
 
     error = co2Sens.startPeriodicMeasurement();
     if (error) {
-        Serial.print("Error starting SCD40 measurement: ");
-        errorToString(error, message, 256);
-        Serial.println(message);
+        printSensirionError("Error starting SCD40 measurement: ", error);
     } else {
         Serial.println("SCD40 measurement started successfully");
     }
 }
 
+// Print an error message and a decoded Sensirion error code
+void printSensirionError(String message, uint16_t error) {
+    char errorMessage[256];
+    errorToString(error, errorMessage, 256);
+    Serial.print(message);
+    Serial.println(errorMessage);
+}
+
+// Initialise the SSD1306 OLED display settings and display a small message
 bool initDisplay() {
     if(!display.begin(SSD1306_SWITCHCAPVCC, DISPLAY_ADDRESS)) {
         Serial.println(F("Couldn't initialise SSD1306"));
@@ -166,7 +166,7 @@ bool initDisplay() {
     display.setTextSize(1);
     // TODO: Uncomment when font fixed
     //display.setFont(&FreeSans9pt7b);
-    display.setTextColor(SSD1306_WHITE);
+    display.setTextColor(SSD1306_WHITE, SSD1306_BLACK); // foreground, background
     display.setCursor(0, 20);
     display.println("display init...");
     display.display();
@@ -212,4 +212,54 @@ void showMessage(String message, message_t level) {
 
     // print to serial, for good measure
     Serial.println(message);
+}
+
+// Show the PM values on the OLED and serial monitor
+void showPMValues(float pm1p0, float pm2p5, float pm4p0, float pm10p0) {
+    Serial.print("PM1.0: ");
+    Serial.print(pm1p0);
+    Serial.print("\t PM2.5: ");
+    Serial.print(pm2p5);
+    Serial.print("\t PM4.0: ");
+    Serial.print(pm4p0);
+    Serial.print("\t PM10.0: ");
+    Serial.print(pm10p0);
+    Serial.println();
+
+    // Only show PM2.5 and PM10 for brevity
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.print("PM2.5: ");
+    display.print(pm2p5);
+    display.println(" ug/m3");
+    display.print("PM10: ");
+    display.print(pm10p0);
+    display.print(" ug/m3");
+    display.display();
+}
+
+// Show the CO2 ppm, temperature and humidity on the OLED and serial monitor
+void showCO2Values(uint16_t co2, float temp, float humi) {
+    Serial.print("CO2: ");
+    Serial.print(co2);
+    Serial.print("\t Temperature: ");
+    Serial.print(temp);
+    Serial.print("\t Humidity: ");
+    Serial.print(humi);
+    Serial.println();
+
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.print("CO2: ");
+    display.print(co2);
+    display.print(" ppm");
+    display.setCursor(0,10);
+    display.print("Temp: ");
+    display.print(temp);
+    display.print("C");
+    display.setCursor(0,20);
+    display.print("Humidity: ");
+    display.print(humi);
+    display.print("%");
+    display.display();
 }
